cdf2c21aea1b4a3bf77c0554f3ff9fad010a754b
Atlante Avila <atlanteavila@gmail.com>
2025-09-30 21:16:09 -0400

    Adds formatting change to functions.php

--- functions.php
+++ functions.php
@@ -5,7 +5,8 @@ add_theme_support('post-thumbnails');
 add_theme_support('menus');
 
 // Register a sample menu
-function hydrated_register_menus() {
+function hydrated_register_menus()
+{
     register_nav_menu('main-header', __('Main Header Menu'));
 }
 add_action('init', 'hydrated_register_menus');
@@ -30,7 +31,7 @@ add_action('init', function () {
 // can turn any Group into a bento grid if they want)
 add_action('init', function () {
     register_block_style('core/group', [
-        'name'  => 'bento-grid',
+        'name' => 'bento-grid',
         'label' => __('Bento Grid', 'hydrated'),
     ]);
 });
